4f3861ab400c52c125a537a87a228756335dfbd9
Myrmex <dfusi@hotmail.com>
2022-01-12 14:59:38 +0100

    Intersect pipe

--- projects/myrmidon/cadmus-text-block-view/src/lib/pipes/array-intersect.pipe.ts
+++ projects/myrmidon/cadmus-text-block-view/src/lib/pipes/array-intersect.pipe.ts
@@ -11,15 +11,26 @@ export class ArrayIntersectPipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
     if (
       !value ||
+      !value.length ||
       !args.length ||
+      !args[0]?.length ||
       !Array.isArray(value) ||
       !Array.isArray(args[0])
     ) {
       return value;
     }
     // array intersection
+    const a: [] = [...(value as [])];
+    const b = new Set(args[0] as []);
+    for (let i = a.length - 1; i > -1; i--) {
+      if (!b.has(a[i])) {
+        a.splice(i, 1);
+      }
+    }
+    return a;
+
     // https://stackoverflow.com/questions/1885557/simplest-code-for-array-intersection-in-javascript
-    const setB = new Set(args[0] as []);
-    return [...new Set(value as [])].filter((x) => setB.has(x));
+    // const setB = new Set(args[0] as []);
+    // return [...new Set(value as [])].filter((x) => setB.has(x));
   }
 }
