1103f6ae66750e216d9e469436d5dc2253f50eb1
Myrmex <dfusi@hotmail.com>
2022-08-04 08:19:39 +0200

    Richer sample for assertion

--- src/app/refs/assertion-pg/assertion-pg.component.ts
+++ src/app/refs/assertion-pg/assertion-pg.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThesaurusEntry } from '@myrmidon/cadmus-core';
 
 import { Assertion } from 'projects/myrmidon/cadmus-refs-assertion/src/public-api';
 
@@ -10,8 +11,32 @@ import { Assertion } from 'projects/myrmidon/cadmus-refs-assertion/src/public-ap
 export class AssertionPgComponent implements OnInit {
   public initialAssertion: Assertion | undefined;
   public assertion: Assertion | undefined;
+  public refTypeEntries: ThesaurusEntry[];
 
-  constructor() {}
+  constructor() {
+    this.refTypeEntries = [
+      {
+        id: 'text',
+        value: 'text',
+      },
+      {
+        id: 'book',
+        value: 'book',
+      },
+      {
+        id: 'biblio',
+        value: 'bibliography',
+      },
+      {
+        id: 'ms',
+        value: 'manuscript',
+      },
+      {
+        id: 'doc',
+        value: 'archive document',
+      },
+    ];
+  }
 
   ngOnInit(): void {}
 
