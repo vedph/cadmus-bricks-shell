4f8107ef4305a74ad9d25855456fc6566b586e5f
Myrmex <dfusi@hotmail.com>
2023-11-09 09:51:02 +0100

    Upgraded to Angular 17

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 // https://www.jvandemo.com/how-to-use-environment-variables-to-configure-your-angular-application-without-a-rebuild/
 (function (window) {
   window.__env = window.__env || {};
-  window.__env.version = "2.0.1";
+  window.__env.version = "3.0.0";
 })(this);
